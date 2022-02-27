33983b3441e5702c572949bf922c75aae6593486
Erik Engheim <erik.engheim@mac.com>
2022-02-28 00:45:16 +0100

    Plotting American resource advantages

--- src/populations.jl
+++ src/populations.jl
@@ -1,5 +1,5 @@
 # Plots related to CPU talks
-export cpuwatts, plotvolts, plottransistors, plotfreq
+export ancient, ironproduction, usa_advantage
 
 using CairoMakie, ElectronDisplay
 
@@ -13,14 +13,89 @@ noto_sans_bold = assetpath("fonts", "NotoSans-Bold.ttf")
 # Greece https://www.cs.mcgill.ca/~rwest/wikispeedia/wpcd/wp/a/Ancient_Greece.htm
 # Egypt https://brewminate.com/estimating-population-in-ancient-egypt/
 function ancient()
-    fig = Figure(resolution = (1024, 768), font = noto_sans)
+    fig = Figure(resolution = (800, 800), font = noto_sans)
     
     # Population around 500 BC
     # Africa here is sub-Saharan Africa
     # Greece with all colonies estimated to 8-10 million so I set it at 9 million    
     populations = Dict("Greece"=>9, "Persia"=>17, "India"=>35, "China"=>43, "Africa"=>7, "Japan"=>0.1, "Egypt" => 5)
-    barplot!(fig, )
+    countries = ["Greece", "Japan", "Africa", "Egypt"]
+    
+    xs = 1:length(countries)
+    ys = [populations[country] for country in countries]
+    axis = Axis(fig[1,1],
+                 xticks = (xs, countries),
+                 ylabel = "millions",
+                 title = "Population (500 BC)")
+    
+    ylims!(axis, (0, 10))
+    
+    barplot!(axis, xs, ys, 
+        color=xs,           # Colors picked from the color table for each bar
+        bar_labels=:y,      # Put value labels on top of each bar
+        width = 0.7         # Width of each of the bars in relation to xs values
+    )
+    
+    fig                     # barplot! does not return something we can show
 end
 
+function ironproduction()
+    fig = Figure(resolution = (1024, 600), font = noto_sans)
+
+    # Source for iron production https://www.statista.com/statistics/1071007/iron-ore-production-europe-1900-1945-country/
+    # Numbers given in million metric tons
+    ironprod = Dict("USA"=> 28.0, "Germany"=>12.7, "France"=>5.5, "UK"=>14.2, "Sweden"=>2.6, "Soviet"=>6.0, "Luxembourg"=>6.1)
+    countries = ["Germany", "UK", "France", "USA"]
+    
+    xs = 1:length(countries)
+    ys = [ironprod[country] for country in countries]
+    
+    axis = Axis(fig[1,1],
+                 xticks = (xs, countries),
+                 ylabel = "million metric tons",
+                 title = "Iron ore production (1900)")
+    
+    ylims!(axis, (0, 30))
+
+    
+    barplot!(axis, xs, ys, 
+        color=xs,          # Colors picked from the color table for each bar
+        bar_labels=:y,     # Put value labels on top of each bar
+        width = 0.7        # Width of each of the bars in relation to xs values
+    )
+                              
+    return fig
+end
+
+function usa_advantage()
+    fig = Figure(resolution = (1024, 600), font = noto_sans)
+
+    # Based on data written at the end of article: https://medium.com/p/9a76c1a12456/edit
+    ratios = Dict("agriculture"=> 6, "coal"=>5.5, "hydropower"=>2.3, "oil"=>150)
+    resources = ["agriculture", "coal", "hydropower"]
+    
+    xs = 1:length(resources)
+    ys = [ratios[r] for r in resources]
+    
+    axis = Axis(fig[1,1],
+                 xticks = (xs, resources),
+                 ylabel = "Ratio",
+                 title = "American resources advantage relative to Europe")
+    
+    ylims!(axis, (0, 6.5))
+
+    
+    barplot!(axis, xs, ys, 
+        color=xs,          # Colors picked from the color table for each bar
+        bar_labels=:y,     # Put value labels on top of each bar
+        width = 0.7        # Width of each of the bars in relation to xs values
+    )
+                              
+    fig    
+end
+
+function usa_marketsize()
+    
+end
 
 # display(ancient())
--- src/rockets.jl
+++ src/rockets.jl
@@ -16,9 +16,10 @@ function plot_neutron_falcon_stats()
                       ylabel = "meters")
             
     # position of each bar along x-axis
+    # repeated x values means several bars are placed in the same position 
     xs =    [1, 1, 1, 2, 2, 2, 3, 3, 3]
     
-    # assigning each bar to a group. 
+    # indicate order for y-values placed on the same x-axis location.
     group = [1, 2, 3, 1, 2, 3, 1, 2, 3]
     
     # y-axis value for each bar
@@ -46,6 +47,9 @@ function plot_mass_stats()
                       ylabel = "tonne")
             
     # position of each bar along x-axis
+    # the max value is 3, which suggests the x-axis goes from 1 - 3
+    # the same x-value is repeated twice, which means two y-values get
+    # placed on the same location at every x-value
     xs =    [1, 1, 2, 2, 3, 3]
     
     # assigning each bar to a group. 
@@ -88,23 +92,46 @@ function plot_other_stats()
 end
 
 function plot_rocket_specs()
-    colors = ColorSchemes.rainbow.colors
+    # Picked from catalog: https://juliagraphics.github.io/ColorSchemes.jl/stable/catalogue/
+    colors = ColorSchemes.Spectral_4.colors
     
     df = load_rocket_specs()
-    fig = Figure(resolution = (1600, 600), font = noto_sans)
+    fig = Figure(resolution = (1024, 1024), font = noto_sans)
     
     cols = [:height, :diameter, :fairing, :mass, :payload, :thrust, :Isp]
     units = ["meter", "meter", "meter", "tonne", "tonne", "kilo Newton", "Secons"]
     
+    # column position in grid for plot for a given index
+    colpos = [1, 1, 1,  # meters
+              2, 2,     # tonnes
+              3, 3]
+    rowpos = [1, 2, 3,
+              1, 2,
+              1, 2]
+    
     rows = nrow(df)
     for (i, col) in enumerate(cols)
-        ax = Axis(fig[1, i], 
-                  ylabel = units[i],
-                  title = string(col))
-        barplot!(ax, 1:rows, df[:, col], color=colors[1:rows])
+        ax = Axis(fig[colpos[i], rowpos[i]], 
+                  ylabel = units[i],    # label along y-axis
+                  title = string(col), 
+                  xticks = 0:0          # num of tick marks and their spacing
+             )
+        # hidedecorations!(ax)
+        # hidespines!(ax)
+        barplot!(ax, 1:rows, df[:, col], 
+            color=colors[1:rows],
+            # color_over_bar=:white,
+            # label_size=12,
+            # flip_labels_at=0,
+            # font=noto_sans_bold,
+            # bar_labels=:y
+        )
+    end
+    for i in 1:3
+        colsize!(fig.layout, i, Relative(1/3))
     end
     elements = polyelement.(colors[1:rows])
-    Legend(fig[2, 1], elements, df[:, :rocket])
+    Legend(fig[2, 3], elements, df[:, :rocket])
     
     fig
 end
